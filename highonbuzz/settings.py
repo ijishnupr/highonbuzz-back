4b7711d8774d5be0afe86121de941af5474f6fb8
jishnu pr <ijishnupr@gmail.com>
2023-11-29 09:01:31 +0530

    mysql

--- highonbuzz/settings.py
+++ highonbuzz/settings.py
@@ -74,16 +74,17 @@ WSGI_APPLICATION = 'highonbuzz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'database', 
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1', 
-        'PORT': '5432',
-    }
-}
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'database',  
+        'USER': 'root',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+          
+    }  
+}  
 
 
 # Password validation
